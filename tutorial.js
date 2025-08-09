3fc7f9e01f4705d161cad82f36f033cd61202b3c
notagentp <yashrajjparmar@outlook.com>
2025-08-09 15:04:29 -0700

    initial commit

---
+++ tutorial.js
@@ -0,0 +1,19 @@
+let str = "A";
+
+let str2 = 'n';
+
+console.log(str.length);
+
+let sentence = "           Two\tWords"
+
+console.log(sentence)
+
+let obj ={
+    item : "pen",
+    price : 10, 
+
+};
+
+let pecial = `The cost of ${obj.item} is ${obj.price}`;
+
+console.log(pecial)
