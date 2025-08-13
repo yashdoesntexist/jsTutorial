b7adcdda534ae5b84dab2ced32f8b338402317a5
yashdoesntexist <learnhacksandtricks@gmail.com>
2025-08-13 09:43:28 -0700

    Starting inheritance

--- OOP/tutorial.js
+++ OOP/tutorial.js
@@ -1,9 +1,51 @@
-const student = {
-    fullName: "Je",
-    grade: 99,
-    printGrade: function(){
-        console.log("Your grade is ", this.grade);
+// const student = {
+//     fullName: "Je",
+//     grade: 99,
+//     printGrade: function(){
+//         console.log("Your grade is ", this.grade);
+//     }
+// }
+
+// student.printGrade();
+
+const employee = {
+    calcTax(){
+        console.log("Tax rate is 3 %");
     }
 }
 
-student.printGrade();
+const bruceWayne = {
+    salary: "2000000",
+};
+
+bruceWayne.__proto__ = employee;
+
+class car{
+    constructor(brand, mileage){
+        this.brand = brand;
+        this.mileage = mileage;
+        console.log("Nice", brand, mileage);
+    }
+    start(){
+        console.log("start");
+    }
+
+    stop(){
+        console.log("stop");
+    }
+
+    // setBrand(brand){
+    //     this.brand = brand;
+
+    // }
+}
+
+let toyota = new car("Camry", 30000);
+// toyota.setBrand("Camry");
+let lexus = new car();
+
+lexus.brand = "c class"; //can set stuff later 
+lexus.mileage = 3000000;
+
+console.log(lexus);
+
