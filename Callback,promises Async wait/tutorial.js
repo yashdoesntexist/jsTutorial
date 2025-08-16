7b9fa0cdea6344c068d1f401127e4d91d37a68aa
yashdoesntexist <learnhacksandtricks@gmail.com>
2025-08-15 18:25:48 -0700

    Aug 15

--- Callback,promises Async wait/tutorial.js
+++ Callback,promises Async wait/tutorial.js
@@ -1,3 +1,66 @@
+console.log("test")
+
 setTimeout(() =>{
     console.log("testing");
 }, 4000);
+
+console.log("tested")  
+
+// callback in sync 
+function sum(a,b) {
+    console.log(a+b);
+}
+
+function Calc(a, b, sumCallBack){
+    sumCallBack(a,b);
+}
+
+Calc(2, 4, sum);
+
+function getData(dataId, getNextData){
+    setTimeout(()=>{
+        console.log("First Data", dataId);
+        if(getNextData){
+            getNextData();
+        }
+    }, 2000);
+}
+// callback Hell 
+getData(12, ()=>{
+    getData(22, ()=>{
+        getData(3)
+    });
+});
+
+
+// Promises
+
+const getPromise = () => {
+    return new Promise((resolve, reject)=>{
+    console.log("promise");
+    // resolve("Success");
+    reject("Some problem that will be printed in .catch");
+});
+}
+
+let promise = getPromise();
+promise.then((res)=>{
+    console.log("promise fullfilled", res);
+});
+promise.catch((err)=>{
+    console.log("rejected", err);
+});
+
+function gettingData(dataInfo, nextData){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log(dataInfo);
+            resolve("Success");
+            if(nextData){
+                nextData();
+            }
+        }, 5000);
+    });
+}
+
+let seeResult = gettingData("Testing promise");
